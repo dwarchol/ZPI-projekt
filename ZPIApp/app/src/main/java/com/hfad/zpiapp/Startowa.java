80484b6f8720db936626e11ec86fdb7744d98ec3
KacperczykKinga <agnikkac@gmail.com>
2020-03-29 03:39:48 +0200

    2 pierwsze layouty
    Zrobiłam layouty: Startowa i Startowa po animacji, ale jeszcze popatrzyłabym na kolory przycisków czy by ich łądniej nie podopasowywać

--- ZPIApp/app/src/main/java/com/hfad/zpiapp/Startowa.java
+++ ZPIApp/app/src/main/java/com/hfad/zpiapp/Startowa.java
@@ -1,7 +1,13 @@
 package com.hfad.zpiapp;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class Startowa extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class Startowa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startowa);
+
+        Button haveAccount = (Button) findViewById(R.id.haveAccount);
+        Button register = (Button) findViewById(R.id.register);
+
+        new CountDownTimer(30000, 100) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+            }
+        }.start();
+
+        moveButton(haveAccount);
+        moveButton(register);
+    }
+
+    public void moveButton(Button button)
+    {
+        DisplayMetrics dm = this.getResources().getDisplayMetrics();
+
+        float yDest = -2*dm.heightPixels/5 ;
+
+        int originalPos[] = new int[2];
+        button.getLocationOnScreen( originalPos );
+
+        TranslateAnimation animation = new TranslateAnimation( originalPos[0], originalPos[0] , originalPos[1], yDest - originalPos[1]);
+        animation.setDuration(1000);
+        animation.setFillAfter( true );
+        button.startAnimation(animation);
     }
 }
