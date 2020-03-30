81a0905178e317ea37b3b5bea1fc6b145f28a81d
p-szeszko <przemekszeszko1@gmail.com>
2020-03-30 22:26:46 +0200

    Revert "Revert "łączenie 2 commitów""
    This reverts commit 7d0d83cd8e369a8039c365c8bfcc9762ee3caf42.

---
+++ ZPIApp/app/src/main/java/com/hfad/zpiapp/Uzytkownik.java
@@ -0,0 +1,17 @@
+package com.hfad.zpiapp;
+
+public class Uzytkownik {
+    String login;
+    String password;
+
+    public Uzytkownik()
+    {
+        login="";
+        password="";
+    }
+    public Uzytkownik(String l, String p)
+    {
+        login=l;
+        password=p;
+    }
+}
